11efdc2617dc32f8eecf30c28a2b7479f19887c6
Sketched <34355327+neatsketched@users.noreply.github.com>
2019-11-04 12:20:11 -0600

    Battle: EXP cap 200 -> 500

--- toontown/toonbase/ToontownBattleGlobals.py
+++ toontown/toonbase/ToontownBattleGlobals.py
@@ -107,7 +107,7 @@ UnpaidMaxSkills = [Levels[0][1] - 1,
  Levels[4][4] - 1,
  Levels[5][4] - 1,
  Levels[6][1] - 1]
-ExperienceCap = 200
+ExperienceCap = 500
 
 def gagIsPaidOnly(track, level):
     return Levels[track][level] > UnpaidMaxSkills[track]
