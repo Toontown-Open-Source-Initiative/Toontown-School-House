da8b553a37c5204703a36a6d3fc94c95ee9e5f3e
DarkHappyy <57161803+DarkHappyy@users.noreply.github.com>
2019-11-04 18:28:54 -0500

    hotfix: suits: mingler hp bugged when merging

--- toontown/battle/SuitBattleGlobals.py
+++ toontown/battle/SuitBattleGlobals.py
@@ -477,7 +477,7 @@ SuitAttributes = {'f': {'name': TTLocalizer.SuitFlunky, # cog name
        'singularname': TTLocalizer.SuitTheMinglerS,
        'pluralname': TTLocalizer.SuitTheMinglerP,
        'level': 0,
-       'hp':(6,12,20,30,42,5672,90,110,132,156),
+       'hp':(6,12,20,30,42,56,72,90,110,132,156),
        'def':(2,5,10,15,20,25,30,35,40,45,50),
        'freq':(0,0,0,0,0,0,50,30,10,5,5),
        'acc':(35,35,35,35,35,35,35,40,45,50,55),
