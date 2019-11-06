a2db11acec3ee98a68936db51f76889aba5403f6
DarkHappyy <57161803+DarkHappyy@users.noreply.github.com>
2019-11-05 23:50:12 -0500

    win32: made new districts, added a second acc

--- win32/start_all.bat
+++ win32/start_all.bat
@@ -1,19 +1,18 @@
 @echo off
 
-taskkill /f /im astrond.exe
-taskkill /f /im ppython.exe
-taskkill /f /fi "windowtitle eq Toontown Online - UberDOG Server"
-taskkill /f /fi "windowtitle eq Toontown Online - Astron Server"
-taskkill /f /fi "windowtitle eq Toontown Online - AI (District) Server"
-taskkill /f /fi "windowtitle eq Toontown Online - Game Client"
-
 start start_astron_server.bat
 
 ping 192.0.2.2 -n 1 -w 300 > nul
 start start_uberdog_server.bat
 
 ping 192.0.2.2 -n 1 -w 300 > nul
-start start_ai_server.bat
+start start_nuttyriver.bat
+
+ping 192.0.2.2 -n 1 -w 300 > nul
+start start_nuttysummit.bat
+
+ping 192.0.2.2 -n 1 -w 300 > nul
+start start_toonvalley.bat
 
 ping 192.0.2.2 -n 1 -w 1500 > nul
 start start_game.bat
