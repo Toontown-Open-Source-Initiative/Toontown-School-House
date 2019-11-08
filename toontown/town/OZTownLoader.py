a2e89d2fbbe220c3588b7102f7facd799147eaa1
Anonymous13 <mgracer48@yahoo.com>
2019-11-08 06:16:33 +0100

    street now loads!

--- toontown/town/OZTownLoader.py
+++ toontown/town/OZTownLoader.py
@@ -10,11 +10,13 @@ class OZTownLoader(TownLoader.TownLoader):
         self.musicFile = 'phase_6/audio/bgm/OZ_SZ.ogg'
         self.activityMusicFile = 'phase_3.5/audio/bgm/TC_SZ_activity.ogg'
         self.townStorageDNAFile = 'phase_6/dna/storage_OZ_town.dna'
+
     def load(self,zoneId):
         TownLoader.TownLoader.load(self,zoneId)
         Suit.loadSuits(3)
-        dnaFile = 'phase_8/dna/outdoor_zone_' + str(self.canonicalBranchZone) + '.dna'
+        dnaFile = 'phase_6/dna/outdoor_zone_' + str(self.canonicalBranchZone) + '.dna'
         self.createHood(dnaFile)
+        
     def unload(self,zoneId):
         Suit.unloadSuits(3)
         TownLoader.TownLoader.unload(self,zoneId)
