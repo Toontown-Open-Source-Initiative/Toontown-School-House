3ef6df819c4696b1277d2d9a454764253405d802
Anonymous13 <mgracer48@yahoo.com>
2019-11-09 16:54:27 +0100

    fix some files up for loading

--- toontown/safezone/FFSafeZoneLoader.py
+++ toontown/safezone/FFSafeZoneLoader.py
@@ -9,8 +9,8 @@ class FFSafeZoneLoader(SafeZoneLoader.SafeZoneLoader):
     def __init__(self,hood,parentFSM,doneEvent):
         SafeZoneLoader.SafeZoneLoader.__init__(self,hood,parentFSM,doneEvent)
         self.playgroundClass = FFPlayground.FFPlayground
-        self.musicFile = 'phase_6/audio/bgm/FF_nbrhood.ogg'
-        self.activityMusicFile = 'phase_6/audio/bgm/FF_SZ_activity.ogg'
+        self.musicFile = 'phase_6/audio/bgm/FF_nbrhood.mp3'
+        self.activityMusicFile = 'phase_6/audio/bgm/FF_SZ_activity.mp3'
         self.dnaFile = 'phase_6/dna/funny_farm_sz.dna'
         self.safeZoneStorageDNAFile = 'phase_6/dna/storage_FF_sz.dna'
     
--- toontown/town/FFTownLoader.py
+++ toontown/town/FFTownLoader.py
@@ -7,8 +7,8 @@ class FFTownLoader(TownLoader.TownLoader):
     def __init__(self,hood,parentFSM,doneEvent):
         TownLoader.TownLoader.__init__(self,hood,parentFSM,doneEvent)
         self.streetClass = FFStreet.FFStreet
-        self.musicFile = 'phase_6/audio/bgm/FF_SZ.ogg'
-        self.activityMusicFile = 'phase_6/audio/bgm/FF_SZ_activity.ogg'
+        self.musicFile = 'phase_6/audio/bgm/FF_SZ.mp3'
+        self.activityMusicFile = 'phase_6/audio/bgm/FF_SZ_activity.mp3'
         self.townStorageDNAFile = 'phase_6/dna/storage_FF_town.dna'
 
     def load(self,zoneId):
