bb727f1e4c63322f6a4eccd099bbaa3551e468b2
Captain Fireball Wildbumper <57166938+CaptainFireball@users.noreply.github.com>
2019-11-12 00:01:30 -0500

    Added VP Model, 3 Goon Models, and 4 Headless Mr. Hollywood Actors to Sellbot HQ Courtyard
    Story Title: The Aftermath of the VP Battle.  Story: After falling from Sellbot Towers the strong Mr. Hollywoods tend their leader Senior V.P. and try to help set him upright in the pit he crashed in.  While this taking place, three Security Goons block the tunnel to Daisy's Gardens to prevent any toons from interfere with the salvage operation.

--- toontown/coghq/SellbotCogHQLoader.py
+++ toontown/coghq/SellbotCogHQLoader.py
@@ -11,6 +11,7 @@ import FactoryInterior
 import SellbotHQExterior
 import SellbotHQBossBattle
 from panda3d.core import DecalEffect
+from direct.actor.Actor import Actor
 aspectSF = 0.7227
 
 class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
@@ -43,6 +44,28 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
         if self.geom:
             self.geom.removeNode()
             self.geom = None
+        self.VPHead.removeNode()
+        del self.VPHead
+        self.VPTorso.removeNode()
+        del self.VPTorso
+        self.VPTank.removeNode()
+        del self.VPTank
+        self.VPTreads.removeNode()
+        del self.VPTreads
+        self.GoonGuard1.removeNode()
+        del self.GoonGuard1
+        self.GoonGuard2.removeNode()
+        del self.GoonGuard2
+        self.GoonGuard3.removeNode()
+        del self.GoonGuard3
+        self.HollySuit1.removeNode()
+        del self.HollySuit1
+        self.HollySuit2.removeNode()
+        del self.HollySuit2
+        self.HollySuit3.removeNode()
+        del self.HollySuit3
+        self.HollySuit4.removeNode()
+        del self.HollySuit4
         CogHQLoader.CogHQLoader.unloadPlaceGeom(self)
         return
 
@@ -51,6 +74,85 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
         zoneId = zoneId - zoneId % 100
         if zoneId == ToontownGlobals.SellbotHQ:
             self.geom = loader.loadModel(self.cogHQExteriorModelPath)
+            self.VPHead = loader.loadModel('phase_9/models/char/sellbotBoss-head-zero')
+            self.VPHead.reparentTo(self.geom)
+            self.VPHead.setPosHpr(0.123, -196.741, -18.594, -1170.522, 0, 0)
+
+            self.VPTorso = loader.loadModel('phase_9/models/char/sellbotBoss-torso-zero')
+            self.VPTorso.reparentTo(self.geom)
+            self.VPTorso.setPosHpr(0.386, -188.850, -19.594, -543.251, -450, 0)
+
+            self.VPTank = loader.loadModel('phase_9/models/char/bossCog-legs-zero')
+            self.VPTank.reparentTo(self.geom)
+            self.VPTank.setPosHpr(0.239, -179.500, -19.594, -543.251, -450, 0)
+
+            self.VPTreads = loader.loadModel('phase_9/models/char/bossCog-treads')
+            self.VPTreads.reparentTo(self.geom)
+            self.VPTreads.setPosHpr(0.239, -179.500, -21, -543.251, -450, 0)
+
+            self.GoonGuard1 = loader.loadModel('phase_9/models/char/Cog_Goonie-zero')
+            self.GoonGuard1.reparentTo(self.geom)
+            self.GoonGuard1.setPosHpr(5.955, -292.554, 1.243, 360.889, 0, 0)
+            self.GoonGuard1.setScale(2)
+
+            self.GoonGuard2 = loader.loadModel('phase_9/models/char/Cog_Goonie-zero')
+            self.GoonGuard2.reparentTo(self.geom)
+            self.GoonGuard2.setPosHpr(-0.562, -292.554, 1.243, 0, 0, 0)
+            self.GoonGuard2.setScale(2)
+
+            self.GoonGuard3 = loader.loadModel('phase_9/models/char/Cog_Goonie-zero')
+            self.GoonGuard3.reparentTo(self.geom)
+            self.GoonGuard3.setPosHpr(-6.057, -292.554, 1.243, 361.054, 0, 0)
+            self.GoonGuard3.setScale(2)
+
+            self.HollySuit1 = Actor('phase_3.5/models/char/SuitA-mod',
+                                    {'suitA-squirt-large': 'phase_5/models/char/suitA-squirt-large'})
+            self.HollySuit1.reparentTo(self.geom)
+            self.HollySuit1.sellBlazer1 = loader.loadTexture('phase_3.5/maps/s_blazer.jpg')
+            self.sellBlazer1 = self.HollySuit1.find('**/torso').setTexture(self.HollySuit1.sellBlazer1, 1)
+            self.HollySuit1.sellLeg1 = loader.loadTexture('phase_3.5/maps/s_leg.jpg')
+            self.sellLeg1 = self.HollySuit1.find('**/legs').setTexture(self.HollySuit1.sellLeg1, 1)
+            self.HollySuit1.sellSleeve1 = loader.loadTexture('phase_3.5/maps/s_sleeve.jpg')
+            self.sellSleeve1 = self.HollySuit1.find('**/arms').setTexture(self.HollySuit1.sellSleeve1, 1)
+            self.HollySuit1.setPosHpr(-1.614, -194.110, -19.594, -27.220, 0, 0)
+            self.HollySuit1.loop('suitA-squirt-large', restart = 0, fromFrame = 20, toFrame = 60)
+
+            self.HollySuit2 = Actor('phase_3.5/models/char/SuitA-mod',
+                                    {'suitA-squirt-large': 'phase_5/models/char/suitA-squirt-large'})
+            self.HollySuit2.reparentTo(self.geom)
+            self.HollySuit2.sellBlazer2 = loader.loadTexture('phase_3.5/maps/s_blazer.jpg')
+            self.sellBlazer2 = self.HollySuit2.find('**/torso').setTexture(self.HollySuit2.sellBlazer2, 1)
+            self.HollySuit2.sellLeg2 = loader.loadTexture('phase_3.5/maps/s_leg.jpg')
+            self.sellLeg2 = self.HollySuit2.find('**/legs').setTexture(self.HollySuit2.sellLeg2, 1)
+            self.HollySuit2.sellSleeve2 = loader.loadTexture('phase_3.5/maps/s_sleeve.jpg')
+            self.sellSleeve2 = self.HollySuit2.find('**/arms').setTexture(self.HollySuit2.sellSleeve2, 1)
+            self.HollySuit2.setPosHpr(-0.229, -194.023, -19.594, -4.022, 0, 0)
+            self.HollySuit2.loop('suitA-squirt-large', restart = 0, fromFrame = 20, toFrame = 60)
+
+            self.HollySuit3 = Actor('phase_3.5/models/char/SuitA-mod',
+                                    {'suitA-squirt-large': 'phase_5/models/char/suitA-squirt-large'})
+            self.HollySuit3.reparentTo(self.geom)
+            self.HollySuit3.sellBlazer3 = loader.loadTexture('phase_3.5/maps/s_blazer.jpg')
+            self.sellBlazer3 = self.HollySuit3.find('**/torso').setTexture(self.HollySuit3.sellBlazer3, 1)
+            self.HollySuit3.sellLeg3 = loader.loadTexture('phase_3.5/maps/s_leg.jpg')
+            self.sellLeg3 = self.HollySuit3.find('**/legs').setTexture(self.HollySuit3.sellLeg3, 1)
+            self.HollySuit3.sellSleeve3 = loader.loadTexture('phase_3.5/maps/s_sleeve.jpg')
+            self.sellSleeve3 = self.HollySuit3.find('**/arms').setTexture(self.HollySuit3.sellSleeve3, 1)
+            self.HollySuit3.setPosHpr(1.892, -194.297, -19.594, 15.977, 0, 0)
+            self.HollySuit3.loop('suitA-squirt-large', restart=0, fromFrame=20, toFrame=60)
+
+            self.HollySuit4 = Actor('phase_3.5/models/char/SuitA-mod',
+                                    {'suitA-squirt-large': 'phase_5/models/char/suitA-squirt-large'})
+            self.HollySuit4.reparentTo(self.geom)
+            self.HollySuit4.sellBlazer4 = loader.loadTexture('phase_3.5/maps/s_blazer.jpg')
+            self.sellBlazer4 = self.HollySuit4.find('**/torso').setTexture(self.HollySuit4.sellBlazer4, 1)
+            self.HollySuit4.sellLeg4 = loader.loadTexture('phase_3.5/maps/s_leg.jpg')
+            self.sellLeg4 = self.HollySuit4.find('**/legs').setTexture(self.HollySuit4.sellLeg4, 1)
+            self.HollySuit4.sellSleeve4 = loader.loadTexture('phase_3.5/maps/s_sleeve.jpg')
+            self.sellSleeve4 = self.HollySuit4.find('**/arms').setTexture(self.HollySuit4.sellSleeve4, 1)
+            self.HollySuit4.setPosHpr(3.398, -194.494, -19.594, 36.625, 0, 0)
+            self.HollySuit4.loop('suitA-squirt-large', restart = 0, fromFrame = 20, toFrame = 60)
+
             dgLinkTunnel = self.geom.find('**/Tunnel1')
             dgLinkTunnel.setName('linktunnel_dg_5316_DNARoot')
             factoryLinkTunnel = self.geom.find('**/Tunnel2')
