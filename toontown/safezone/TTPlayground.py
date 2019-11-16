fbf660a0f00f88041e65b6fd06c1736929f9edd9
CyboricBunny <elijahmiles18@outlook.com>
2019-11-16 11:53:14 -0500

    Toontown Central: Changed the position of the house and Horror Show, and added decorations to the area around the house

--- toontown/safezone/TTPlayground.py
+++ toontown/safezone/TTPlayground.py
@@ -16,18 +16,32 @@ class TTPlayground(Playground.Playground):
         
         self.houseB     = loader.loadModel("phase_5.5/models/estate/houseB")
         self.houseB.reparentTo(render)
-        self.houseB.setPos(138, 80, 2.5)
+        self.houseB.setPos(138, 88, 2.5)
         self.houseB.setHpr(180, 0, 0)
         
         self.houseBDoor = loader.loadModel("phase_3.5/models/modules/doors_practical.bam").find("**/door_double_round_ur")
         self.houseBDoor.reparentTo(self.houseB.find("**/door_origin"))
-        self.houseBDoor.setHpr(90, 0, 0)
+        self.houseBDoor.setH(90)
         self.houseBDoor.setScale(0.8)
         self.houseBDoor.setColorScale(0.88, 0.45, 0.38, 1)
+
+        self.planterL = loader.loadModel("phase_5.5/models/estate/planterA")
+        self.planterL.reparentTo(render)
+        self.planterL.setPos(142, 98, 2.5)
+
+        self.planterR = loader.loadModel("phase_5.5/models/estate/planterA")
+        self.planterR.reparentTo(render)
+        self.planterR.setPos(142, 78, 2.5)
+
+        self.wreath = loader.loadModel("phase_5.5/models/estate/tt_m_prp_int_winter_wreath")
+        self.wreath.reparentTo(self.houseB.find("**/door_origin"))
+        self.wreath.setPos(0.2, 0, 4)
+        self.wreath.setH(90)
+        self.wreath.setScale(0.6)
         
         # prop "HouseBGroup_DNARoot" [
         #  code [ "HouseBGroup" ]
-        #  pos [ 138 80 2.5 ]
+        #  pos [ 138 88 2.5 ]
         #  nhpr [ 180 0 0 ]
         #  door [
         #   code [ "door_double_round_ur" ]
@@ -37,14 +51,21 @@ class TTPlayground(Playground.Playground):
         
         # place_model "phase_5.5/models/estate/houseB" [
 	    #     store_node [ "prop" "HouseBGroup" ]
+	    #     ]
 
     def unload(self):
         Playground.Playground.unload(self)
         
         self.houseB.removeNode()
         self.houseBDoor.removeNode()
+        self.planterL.removeNode()
+        self.planterR.removeNode()
+        self.wreath.removeNode()
         del self.houseB
         del self.houseBDoor
+        del self.planterL
+        del self.planterR
+        del self.wreath
 
     def enter(self, requestStatus):
         Playground.Playground.enter(self, requestStatus)
