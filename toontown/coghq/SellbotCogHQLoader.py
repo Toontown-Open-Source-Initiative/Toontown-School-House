639ca93389c7b85274627a0bab6aea790d571a4f
GREENCAT1337 <jahtani66@gmail.com>
2019-11-16 12:57:31 -0800

    Sellbot HQ: added body of water
    Added a body of water to SBHQ, as well as some crates within it.
    
    Issues:
    -Toon doesn't swim for the first submerge
    -Either checkToonUnderwater or submergeToon doesn't work. Currently, swimming is enabled by the camera being underwater or not. That being said, if the camera get's stuck above water, the toon will not start swimming.
    -Underwater audio doesn't stop if the toon leaves the area without emerging from the water.
    -Other issues

--- toontown/coghq/SellbotCogHQLoader.py
+++ toontown/coghq/SellbotCogHQLoader.py
@@ -4,6 +4,7 @@ import CogHQLoader
 from toontown.toonbase import ToontownGlobals
 from direct.gui import DirectGui
 from toontown.toonbase import TTLocalizer
+#from direct.task.Task import Task
 from toontown.toon import Toon
 from direct.fsm import State
 import FactoryExterior
@@ -33,11 +34,15 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
         self.cogHQLobbyModelPath = 'phase_9/models/cogHQ/SellbotHQLobby'
         self.factoryExteriorModelPath = 'phase_9/models/cogHQ/SellbotFactoryExterior'
         self.geom = None
+#        self.ground = ground
         return
 
     def load(self, zoneId):
         CogHQLoader.CogHQLoader.load(self, zoneId)
         Toon.loadSellbotHQAnims()
+        self.underwaterSound = base.loader.loadSfx('phase_4/audio/sfx/AV_ambient_water.ogg')
+        self.swimSound = base.loader.loadSfx('phase_4/audio/sfx/AV_swim_single_stroke.ogg')
+        self.submergeSound = base.loader.loadSfx('phase_5.5/audio/sfx/AV_jump_in_water.ogg')
 
     def unloadPlaceGeom(self):
         if self.geom:
@@ -51,6 +56,7 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
         zoneId = zoneId - zoneId % 100
         if zoneId == ToontownGlobals.SellbotHQ:
             self.geom = loader.loadModel(self.cogHQExteriorModelPath)
+            ground = self.geom.find('**/polySurface81')
             dgLinkTunnel = self.geom.find('**/Tunnel1')
             dgLinkTunnel.setName('linktunnel_dg_5316_DNARoot')
             factoryLinkTunnel = self.geom.find('**/Tunnel2')
@@ -86,6 +92,107 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
                 doorFrame.node().setEffect(DecalEffect.make())
                 index += 1
 
+            self.acidPool = loader.loadModel('phase_14/models/cogHQ/acidPool')
+            self.acidPool.reparentTo(self.geom)
+            self.acidPool.setPos(4, -190, -1)
+            self.acidPool.setScale(1.5)
+            self.acid = loader.loadTexture('phase_14/maps/inkAnim_1.png')
+            self.acidPool.setTexture(self.acid, 1)
+            self.acidPool.setTransparency(1)
+            self.acidPool.setColorScale(1, 1, 1, 0.98)
+
+            self.crate = loader.loadTexture('phase_14/maps/CrateByShadowrunner27.jpg')
+
+            self.crate_1 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_1.setTexture(self.crate, 1)
+            self.crate_1.reparentTo(self.geom)
+            self.crate_1.setPosHprScale(1.1, -155, -3, -8, 0.5, -3.5, 0.92, 0.92, 0.92)
+
+            self.crate_2 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_2.setTexture(self.crate, 1)
+            self.crate_2.reparentTo(self.geom)
+            self.crate_2.setPosHprScale(-13, -166, -5, 8.5, -12, 1, 1, 1, 1)
+
+            self.crate_3 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_3.setTexture(self.crate, 1)
+            self.crate_3.reparentTo(self.geom)
+            self.crate_3.setPosHprScale(34, -172, -5.4, 5.2, 80, 6, 1.2, 1.2, 1.2)
+
+            self.crate_4 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_4.setTexture(self.crate, 1)
+            self.crate_4.reparentTo(self.geom)
+            self.crate_4.setPosHprScale(-30, -164, -4, -0.8, 37, 6.8, 1, 1, 1)
+
+            self.crate_5 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_5.setTexture(self.crate, 1)
+            self.crate_5.reparentTo(self.geom)
+            self.crate_5.setPosHprScale(48, -170, -4, -32, -73, -37, 1, 1, 1)
+
+            self.crate_6 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_6.setTexture(self.crate, 1)
+            self.crate_6.reparentTo(self.geom)
+            self.crate_6.setPosHprScale(1.7, -180, -4.7, -6.5, -8, 4.4, 1.2, 1.2, 1.2)
+
+            self.crate_7 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_7.setTexture(self.crate, 1)
+            self.crate_7.reparentTo(self.geom)
+            self.crate_7.setPosHprScale(21.4, -190, -5.35, -8.5, -25.4, 25.1, 1.2, 1.2, 1.2)
+
+            self.crate_8 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_8.setTexture(self.crate, 1)
+            self.crate_8.reparentTo(self.geom)
+            self.crate_8.setPosHprScale(-26.5, -181, -4.7, -5.9, -9.9, 5.5, 1, 1, 1)
+
+            self.crate_9 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_9.setTexture(self.crate, 1)
+            self.crate_9.reparentTo(self.geom)
+            self.crate_9.setPosHprScale(-42.2, -197, -4.76, -12, 10, 0.9, 1, 1, 1)
+
+            self.crate_10 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_10.setTexture(self.crate, 1)
+            self.crate_10.reparentTo(self.geom)
+            self.crate_10.setPosHprScale(-11.1, -197, -5.34, -1.6, -19.3, -7.7, 0.9, 0.9, 0.9)
+
+            self.crate_11 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_11.setTexture(self.crate, 1)
+            self.crate_11.reparentTo(self.geom)
+            self.crate_11.setPosHprScale(44, 210.8, -6, -65.3, -81.4, -54.6, 1, 1, 1)
+
+            self.crate_12 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_12.setTexture(self.crate, 1)
+            self.crate_12.reparentTo(self.geom)
+            self.crate_12.setPosHprScale(23.2, -208.9, -5.34, -3.5, 62, -0.7, 1.1, 1.1, 1.1)
+
+            self.crate_13 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_13.setTexture(self.crate, 1)
+            self.crate_13.reparentTo(self.geom)
+            self.crate_13.setPosHprScale(3.8, -209.75, -5.34, -94.7, 73.2, -105, 1, 1, 1)
+
+            self.crate_14 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_14.setTexture(self.crate, 1)
+            self.crate_14.reparentTo(self.geom)
+            self.crate_14.setPosHprScale(31.69, -223.85, -5.25, 190.7, 54, 188, 0.8, 0.8, 0.8)
+
+#            self.crate_15 = loader.loadModel('phase_14/models/props/crate')
+#            self.crate_15.setTexture(self.crate, 1)
+#            self.crate_15.reparentTo(self.geom)
+#            self.crate_15.setPosHprScale(-6.66, -223.66 -5.34, -2.8, 4.7, 8, 0.9, 0.9, 0.9)
+
+            self.crate_16 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_16.setTexture(self.crate, 1)
+            self.crate_16.reparentTo(self.geom)
+            self.crate_16.setPosHprScale(-6.5, -230.64, -5.34, 10.4, -41.4, -12, 1, 1, 1)
+
+            self.crate_17 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_17.setTexture(self.crate, 1)
+            self.crate_17.reparentTo(self.geom)
+            self.crate_17.setPosHprScale(-23.9, -211.05, -4.75, -9.8, 16.6, 0, 1.3, .3, 1.3)
+
+            self.crate_18 = loader.loadModel('phase_14/models/props/crate')
+            self.crate_18.setTexture(self.crate, 1)
+            self.crate_18.reparentTo(self.geom)
+            self.crate_18.setPosHprScale(-44.85, -212.6, -4.75, 3.69, 58.15, 1.5, 1.15, 1.15, 1.15)
+
         elif zoneId == ToontownGlobals.SellbotFactoryExt:
             self.geom = loader.loadModel(self.factoryExteriorModelPath)
             factoryLinkTunnel = self.geom.find('**/tunnel_group2')
@@ -94,7 +201,7 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
             cogSignModel = loader.loadModel('phase_4/models/props/sign_sellBotHeadHQ')
             cogSign = cogSignModel.find('**/sign_sellBotHeadHQ')
             cogSignSF = 23
-            elevatorSignSF = 15
+            elevatorSignSF = 1
             hqSign = cogSign.copyTo(factoryLinkTunnel)
             hqSign.setPosHprScale(0.0, -353, 27.5, -180.0, 0.0, 0.0, cogSignSF, cogSignSF, cogSignSF * aspectSF)
             hqSign.node().setEffect(DecalEffect.make())
@@ -167,3 +274,21 @@ class SellbotCogHQLoader(CogHQLoader.CogHQLoader):
 
     def getBossPlaceClass(self):
         return SellbotHQBossBattle.SellbotHQBossBattle
+'''
+    def checkTintTrue(self, task):
+        if  self.groundTint == 1:
+            self.addGroundTint()
+        else:
+            self.removeGroundTint()
+        return Task.cont
+
+    def addGroundTint(self):
+        self.ground = self.geom.find('**/polySurface81')
+        self.ground.setColorScale(66/255, 46/255, 146/255, 1.0)
+
+    def removeGroundTint(self):
+        if self.groundTint == 0:
+            return
+        self.ground = self.geom.find('**/polySurface81')
+        self.ground.clearColorScale()
+'''
--- toontown/hood/SellbotHQ.py
+++ toontown/hood/SellbotHQ.py
@@ -1,8 +1,14 @@
 import CogHood
+from panda3d.core import *
 from toontown.toonbase import ToontownGlobals
 from toontown.coghq import SellbotCogHQLoader
+from direct.task.Task import Task
+from direct.fsm import State
+from toontown.safezone import PublicWalk
+from direct.fsm import ClassicFSM, State
 
 class SellbotHQ(CogHood.CogHood):
+    waterLevel = -1
 
     def __init__(self, parentFSM, doneEvent, dnaStore, hoodId):
         CogHood.CogHood.__init__(self, parentFSM, doneEvent, dnaStore, hoodId)
@@ -11,12 +17,23 @@ class SellbotHQ(CogHood.CogHood):
         self.storageDNAFile = None
         self.skyFile = 'phase_9/models/cogHQ/cog_sky'
         self.titleColor = (0.5, 0.5, 0.5, 1.0)
+        self.cameraSubmerged = 0
+        self.toonSubmerged = 0
+        self.underwaterFogColor = Vec4(30/255, 0, 105/255, 1.0)
+        self.whiteFogColor = Vec4(0.8, 0.8, 0.8, 1.0)
+#        ground = self.geom.find('**/polySurface81')
+#        self.activityFsm = ClassicFSM.ClassicFSM('Activity', [State.State('off', self.enterOff, self.exitOff)
+#        self.activityFsm.enterInitialState()
         return
 
     def load(self):
         CogHood.CogHood.load(self)
         self.sky.setScale(2.0)
+        self.walkDoneEvent = 'walkDone'
         self.parentFSM.getStateNamed('SellbotHQ').addChild(self.fsm)
+        self.walkStateData = PublicWalk.PublicWalk(self.fsm, self.walkDoneEvent)
+        self.walkStateData.load()
+        self.fog = Fog('SBHQFog')
 
     def unload(self):
         self.parentFSM.getStateNamed('SellbotHQ').removeChild(self.fsm)
@@ -32,3 +49,87 @@ class SellbotHQ(CogHood.CogHood):
         localAvatar.setCameraFov(ToontownGlobals.DefaultCameraFov)
         base.camLens.setNearFar(ToontownGlobals.DefaultCameraNear, ToontownGlobals.DefaultCameraFar)
         CogHood.CogHood.exit(self)
+        taskMgr.remove('sbhq-check-toon-underwater')
+        taskMgr.remove('sbhq-check-cam-underwater')
+
+    def enterStart(self):
+        self.cameraSubmerged = 0
+        self.toonSubmerged = 0
+        taskMgr.add(self.__checkToonUnderwater, 'sbhq-check-toon-underwater')
+        taskMgr.add(self.__checkCameraUnderwater, 'sbhq-check-cam-underwater')
+
+
+    def __checkCameraUnderwater(self, task):
+        if camera.getZ(render) < self.waterLevel:
+            self.__submergeCamera()
+        else:
+            self.__emergeCamera()
+        return Task.cont
+
+    def __checkToonUnderwater(self, task):
+        if base.localAvatar.getZ() < self.waterLevel:
+            self.__submergeToon()
+        else:
+            self.__emergeToon()
+        return Task.cont
+
+    def __submergeCamera(self):
+        if self.cameraSubmerged == 1:
+            return
+#        self.ground.setColorScale(66/255, 46/255, 146/255, 1.0)
+        self.groundTint = 1
+        self.loader.hood.setUnderwaterFog()
+        self.walkStateData.fsm.request('swimming', [self.loader.swimSound])
+        base.playSfx(self.loader.underwaterSound, looping=1, volume=0.8)
+        self.cameraSubmerged = 1
+        self.walkStateData.setSwimSoundAudible(1)
+
+    def __emergeCamera(self):
+#        self.ground.clearColorScale()
+        if self.cameraSubmerged == 0:
+            return
+        self.loader.hood.setNoFog()
+#        self.loader.hood.setNoFog()
+        self.walkStateData.fsm.request('walking')
+        self.loader.underwaterSound.stop()
+        self.cameraSubmerged = 0
+        self.groundTint = 0
+        self.walkStateData.setSwimSoundAudible(0)
+
+    def __submergeToon(self):
+        if self.toonSubmerged == 1:
+            return
+        base.playSfx(self.loader.submergeSound)
+        if base.config.GetBool('disable-flying-glitch') == 0:
+#            self.fsm.request('walk')
+            return
+#        self.walkStateData.fsm.request('swimming', [self.loader.swimSound])
+        pos = base.localAvatar.getPos(render)
+        base.localAvatar.d_playSplashEffect(pos[0], pos[1], self.waterLevel)
+#        base.localAvatar.b_setAnimState('swim', 1.0)
+        self.toonSubmerged = 1
+
+    def __emergeToon(self):
+        if self.toonSubmerged == 0:
+            return
+#        self.walkStateData.fsm.request('walking')
+        self.toonSubmerged = 0
+
+    def setUnderwaterFog(self):
+        if base.wantFog:
+            self.fog.setColor(self.underwaterFogColor)
+            self.fog.setLinearRange(0.1, 60)
+            render.setFog(self.fog)
+            self.sky.setFog(self.fog)
+
+    def setNoFog(self):
+        if base.wantFog:
+            render.clearFog()
+            self.sky.clearFog()
+
+'''
+    def enterOff(self):
+        return None
+
+    def exitOff(self):
+        return None'''
