445175db7a32172b39ac952f254dd137460a8535
Anonymous13 <mgracer48@yahoo.com>
2019-12-01 07:25:41 +0200

    NPCFriendPnael: Increase sos card max stars to 10

--- toontown/toon/NPCFriendPanel.py
+++ toontown/toon/NPCFriendPanel.py
@@ -69,7 +69,7 @@ class NPCFriendPanel(DirectFrame):
 
 class NPCFriendCard(DirectFrame):
     normalTextColor = (0.3, 0.25, 0.2, 1)
-    maxRarity = 5
+    maxRarity = 10
     sosTracks = ToontownBattleGlobals.Tracks + ToontownBattleGlobals.NPCTracks
 
     def __init__(self, parent = aspect2dp, rotateCard = False, **kw):
