a1de397fd952786aca083b351607fac62e14111f
midgetalchemist <48665387+midgetalchemist@users.noreply.github.com>
2019-12-01 20:12:26 -0800

    picnic gone wrong animation
    kart flies from nowhere, crashes, food falls

--- toontown/safezone/DDPlayground.py
+++ toontown/safezone/DDPlayground.py
@@ -7,6 +7,7 @@ from direct.actor import Actor
 from toontown.toonbase import ToontownGlobals
 from direct.directnotify import DirectNotifyGlobal
 from toontown.hood import Place
+from direct.interval.IntervalGlobal import *
 
 class DDPlayground(Playground.Playground):
     notify = DirectNotifyGlobal.directNotify.newCategory('DDPlayground')
@@ -31,12 +32,28 @@ class DDPlayground(Playground.Playground):
         self.apple.reparentTo(self.piano)   # males apple a child node of piano
         self.apple.setScale(5)      # sets apple scale to 5
 
+        self.sequence = Sequence(
+            LerpScaleInterval(self.piano, 1.0, 5.0),
+            LerpPosInterval(self.piano, 1.0, (0, 0, 15)),
+            Lerp.PosInterval(self.piano, 1.0, (0, 0, 10)),
+            Wait(5.0),
+            LerpScaleInterval(self.piano, 1.0, 1.0),
+            Wait(5.0)
+        )
+
+        self.sequence.loop()
+
     def unload(self):
+        self.apple.removeNode()
+        del self.piano
         self.piano.removeNode()
         del self.piano
 
         del self.activityFsm
         Playground.Playground.unload(self)
+
+        self.sequence.finish()
+        del self.sequence
 
     def enter(self, requestStatus):
         self.nextSeagullTime = 0
--- toontown/safezone/TTPlayground.py
+++ toontown/safezone/TTPlayground.py
@@ -5,57 +5,100 @@ import random
 from toontown.launcher import DownloadForceAcknowledge
 from direct.task.Task import Task
 from toontown.hood import ZoneUtil
+from direct.interval.IntervalGlobal import *
 
 class TTPlayground(Playground.Playground):
 
     def __init__(self, loader, parentFSM, doneEvent):
         Playground.Playground.__init__(self, loader, parentFSM, doneEvent)
+        self.interval = None
 
     def load(self):
         Playground.Playground.load(self)
         self.foodkart = loader.loadModel("phase_6/models/golf/golf_cart3")
         self.foodkart.reparentTo(render)
-        self.foodkart.setPosHpr(-93.961, -88.638, 10.525, 0, 0, 45)
+        self.foodkart.setPos(0, 0, 50)
+
+        propNode = self.foodkart.attachNewNode('prop')
+        propNode.hide()
 
         self.cupcake1 = loader.loadModel("phase_6/models/golf/picnic_cupcake")
-        self.cupcake1.reparentTo(render)
-        self.cupcake1.setPosHpr(-87.961, -80.638, 9.525, 0, 0, 45)
+        self.cupcake1.reparentTo(propNode)
         self.cupcake1.setScale(5)
         self.cupcake2 = loader.loadModel("phase_6/models/golf/picnic_cupcake")
-        self.cupcake2.reparentTo(render)
-        self.cupcake2.setPosHpr(-92.961, -80.638, 10.250, 0, 0, 135)
+        self.cupcake2.reparentTo(propNode)
         self.cupcake2.setScale(5)
         self.cupcake3 = loader.loadModel("phase_6/models/golf/picnic_cupcake")
-        self.cupcake3.reparentTo(render)
-        self.cupcake3.setPosHpr(-93, -77, 1.850, 0, 0, 165)
+        self.cupcake3.reparentTo(propNode)
         self.cupcake3.setScale(5)
 
         self.choccake = loader.loadModel("phase_6/models/golf/picnic_chocolate_cake")
-        self.choccake.reparentTo(render)
-        self.choccake.setPosHpr(-73.374, -76.127, 2, 0, 0, 180)
+        self.choccake.reparentTo(propNode)
         self.choccake.setScale(5)
 
         self.sandwich1 = loader.loadModel("phase_6/models/golf/picnic_sandwich")
-        self.sandwich1.reparentTo(render)
-        self.sandwich1.setPos(-88, -79.266, 0.650)
+        self.sandwich1.reparentTo(propNode)
         self.sandwich1.setScale(5)
         self.sandwich2 = loader.loadModel("phase_6/models/golf/picnic_sandwich")
-        self.sandwich2.reparentTo(render)
-        self.sandwich2.setPos(-85.513, -79.266, 0.650)
+        self.sandwich2.reparentTo(propNode)
         self.sandwich2.setScale(5)
         self.sandwich3 = loader.loadModel("phase_6/models/golf/picnic_sandwich")
-        self.sandwich3.reparentTo(render)
-        self.sandwich3.setPos(-86.756, -79.266, 1.225)
+        self.sandwich3.reparentTo(propNode)
         self.sandwich3.setScale(5)
 
         self.basket = loader.loadModel("phase_6/models/golf/picnic_basket")
-        self.basket.reparentTo(render)
-        self.basket.setPosHpr(-73.400, -82.848, 3.525, 0, 0, 145)
+        self.basket.reparentTo(propNode)
         self.basket.setScale(5)
 
+        self.interval = Sequence(
+            LerpPosInterval(self.foodkart, 3.5, (-93.961, -88.638, 10.525)),
+            Func(propNode.show),
+            Func(self.cupcake1.wrtReparentTo, render),
+            Func(self.cupcake2.wrtReparentTo, render),
+            Func(self.cupcake3.wrtReparentTo, render),
+            Func(self.choccake.wrtReparentTo, render),
+            Func(self.sandwich1.wrtReparentTo, render),
+            Func(self.sandwich2.wrtReparentTo, render),
+            Func(self.sandwich3.wrtReparentTo, render),
+            Func(self.basket.wrtReparentTo, render),
+            Parallel(
+                LerpPosHprInterval(self.cupcake1, 1.0, (-87.961, -80.638, 9.525), (0, 0, 45)),
+                LerpPosHprInterval(self.cupcake2, 1.0, (-92.961, -80.638, 10.250), (0, 0, 135)),
+                LerpPosHprInterval(self.cupcake3, 1.0, (-93, -77, 1.850), (0, 0, 165)),
+                LerpPosHprInterval(self.choccake, 1.0, (-73.374, -76.127, 2), (0, 0, 180)),
+                LerpPosInterval(self.sandwich1, 1.0, (-88, -79.266, 0.650)),
+                LerpPosInterval(self.sandwich2, 1.0, (-85.513, -79.266, 0.650)),
+                LerpPosInterval(self.sandwich3, 1.0, (-86.756, -79.266, 1.225)),
+                LerpPosHprInterval(self.basket, 1.0, (-73.400, -82.848, 3.525), (0, 0, 145))
+            ),
+            Wait(5.0),
+            Func(self.foodkart.setPos, 0, 0, 200),
+            Func(propNode.hide),
+            Func(self.cupcake1.reparentTo, propNode),
+            Func(self.cupcake2.reparentTo, propNode),
+            Func(self.cupcake3.reparentTo, propNode),
+            Func(self.choccake.reparentTo, propNode),
+            Func(self.sandwich1.reparentTo, propNode),
+            Func(self.sandwich2.reparentTo, propNode),
+            Func(self.sandwich3.reparentTo, propNode),
+            Func(self.basket.reparentTo, propNode),
+            Func(self.cupcake1.setPos, 0, 0, 0),
+            Func(self.cupcake2.setPos, 0, 0, 0),
+            Func(self.cupcake3.setPos, 0, 0, 0),
+            Func(self.choccake.setPos, 0, 0, 0),
+            Func(self.sandwich1.setPos, 0, 0, 0),
+            Func(self.sandwich2.setPos, 0, 0, 0),
+            Func(self.sandwich3.setPos, 0, 0, 0),
+            Func(self.basket.setPos, 0, 0, 0),
+        )
+        self.interval.loop()
 
     def unload(self):
         Playground.Playground.unload(self)
+
+        if self.interval:
+            self.interval.finish()
+            del self.interval
 
     def enter(self, requestStatus):
         Playground.Playground.enter(self, requestStatus)
