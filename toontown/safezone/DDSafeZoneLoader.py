79be0cfd49cd897c58d9b58df8e3b479f202f1f9
Dustin <dustinblakewhite@gmail.com>
2019-12-09 18:33:24 -0600

    clownFish1 is now swimming and moving normally
    must provide detail later

--- toontown/safezone/DDSafeZoneLoader.py
+++ toontown/safezone/DDSafeZoneLoader.py
@@ -43,36 +43,64 @@ class DDSafeZoneLoader(SafeZoneLoader.SafeZoneLoader):
         self.bellSound = base.loader.loadSfx('phase_6/audio/sfx/SZ_DD_shipbell.ogg')
         self.waterSound = base.loader.loadSfx('phase_6/audio/sfx/SZ_DD_waterlap.ogg')
 
+
         clownFishModel = loader.loadModel("phase_4/models/char/clownFish-zero.bam")
         clownFishModel.reparentTo(render)
         self.clownFish = Actor.Actor(clownFishModel, copy=0)
         self.clownFish.reparentTo(render)
         self.clownFish.setPos(-11.403, -48.344, -9.308)
-        self.clownFish.setScale(0.5)
+        self.clownFish.setScale(0.4)
         self.clownFish.setHpr(255, 0, 0)
         self.clownFish.loadAnims({'anim': 'phase_4/models/char/clownFish-swim.bam'})
         self.clownFish.loop('anim')
 
-        self.clownFishRotation = Sequence(
-            LerpHprInterval(self.clownFish, 1, (260, 0, 0)),
-            LerpHprInterval(self.clownFish, 1, (250, 0, 0))
+        clownFishModel2 = loader.loadModel("phase_4/models/char/clownFish-zero.bam")
+        clownFishModel2.reparentTo(render)
+        self.clownFish2 = Actor.Actor(clownFishModel2, copy=0)
+        self.clownFish2.reparentTo(render)
+        self.clownFish2.setPos(-10.403, -48.344, -8.308)
+        self.clownFish2.setScale(0.4)
+        self.clownFish2.setHpr(255, 0, 0)
+        self.clownFish2.loadAnims({'anim': 'phase_4/models/char/clownFish-swim.bam'})
+        self.clownFish2.loop('anim')
+        #below checks number  of frames of an animation
+        #numFrames = self.clownFish2.getNumFrames('anim')
+
+        self.clownFish1Move = Sequence(
+            LerpPosInterval(self.clownFish, 6, (-74.021, -32.160, -9.308)),
+            LerpPosInterval(self.clownFish, 6, (-62.701, 16.049, -9.308)),
+            LerpPosInterval(self.clownFish, 6, (-22.759, 54.937, -9.308)),
+            LerpPosInterval(self.clownFish, 4, (9.270, 37.066, -9.308)),
+            LerpPosInterval(self.clownFish, 6, (-2.356, -37.253, -9.308)),
+            LerpPosInterval(self.clownFish, 1, (-11.403, -48.344, -9.308)),
         )
-        self.clownFishRotation.loop()
 
-        self.clownFishMove = Sequence(
-            LerpPosInterval(self.clownFish, 10, (-74.021, -32.160, -9.308))
+        self.clownFish1Rotate = Sequence(
+            Wait(4),
+            LerpHprInterval(self.clownFish, 2, (165, 0, 0)),
+            Wait(4),
+            LerpHprInterval(self.clownFish, 2, (140, 0, 0)),
+            Wait(4),
+            LerpHprInterval(self.clownFish, 2, (68, 0, 0)),
+            Wait(3),
+            LerpHprInterval(self.clownFish, 1, (-22, 0, 0)),
+            Wait(4),
+            LerpHprInterval(self.clownFish, 2, (-65, 0, 0)),
+            LerpHprInterval(self.clownFish, 1, (-105, 0, 0))
         )
-        self.clownFishMove.loop()
 
+        self.clownFish2Move = Sequence(
+            LerpPosInterval(self.clownFish2, 6, (-73.021, -32.160, -8.308)),
+            LerpPosInterval(self.clownFish2, 6, (-61.021, 16.049, -8.308)),
+            LerpPosInterval(self.clownFish2, 6, (-21.759, 54.937, -8.308)),
+            LerpPosInterval(self.clownFish2, 6, (10.270, 38.066, -8.308)),
+            LerpPosInterval(self.clownFish2, 6, (-1.356, -36.253, -8.308)),
+            LerpPosInterval(self.clownFish2, 1.5, (-10.403, -48.344, -8.308))
+        )
 
-        #clownFishModel2 = loader.loadModel("phase_4/models/char/clownFish-zero.bam")
-        #clownFishModel2.reparentTo(render)
-        #clownFish2 = Actor.Actor(clownFishModel2, copy=0)
-        #clownFish2.reparentTo(render)
-        #clownFish2.setPos(-11.403, -48.344, -6.308)
-        #clownFish2.setScale(0.5)
-        #clownFish2.loadAnims({'anim': 'phase_4/models/char/clownFish-swim.bam'})
-        #clownFish2.loop('anim')
+        self.clownFish1Move.loop()
+        self.clownFish1Rotate.loop()
+        #self.clownFish2Move.loop()
 
     def unload(self):
         SafeZoneLoader.SafeZoneLoader.unload(self)
@@ -87,7 +115,9 @@ class DDSafeZoneLoader(SafeZoneLoader.SafeZoneLoader):
         del self.boat
 
         self.clownFish.removeNode()
+        self.clownFish2.removeNode()
         del self.clownFish
+        del self.clownFish2
 
 
     def enter(self, requestStatus):
--- toontown/safezone/MMSafeZoneLoader.py
+++ toontown/safezone/MMSafeZoneLoader.py
@@ -26,44 +26,44 @@ class MMSafeZoneLoader(SafeZoneLoader.SafeZoneLoader):
             hq.wrtReparentTo(self.piano)
 
         # Yugioh
-        self.blueEyes = loader.loadModel('phase_6/models/extras/bams/BlueEyesWhiteDragon')
-        self.blueEyes.reparentTo(render)
-        self.blueEyes.setPos(62.999, -21.119, 40)
+        #self.blueEyes = loader.loadModel('phase_6/models/extras/bams/BlueEyesWhiteDragon')
+        #self.blueEyes.reparentTo(render)
+        #self.blueEyes.setPos(62.999, -21.119, 40)
 
-        self.sequence = Sequence(
-            LerpPosInterval(self.blueEyes, 2.5, (62.999, -21.119, 60)),
-            Wait(0.25),
-            LerpPosInterval(self.blueEyes, 2.5, (62.999, -21.119, 40))
-        )
-        self.sequence.loop()
+        #self.sequence = Sequence(
+        #    LerpPosInterval(self.blueEyes, 2.5, (62.999, -21.119, 60)),
+        #    Wait(0.25),
+        #    LerpPosInterval(self.blueEyes, 2.5, (62.999, -21.119, 40))
+        #)
+        #self.sequence.loop()
     # Star Wars
-        self.bb8 = loader.loadModel('phase_6/models/extras/bams/BB8')
-        self.bb8.reparentTo(render)
-        self.bb8.setPos(64.199, -98.559, -14.481)
-        self.bb8.setScale(0.25)
-        self.bb8.setHpr(-349.820, 0, 0)
+        #self.bb8 = loader.loadModel('phase_6/models/extras/bams/BB8')
+        #self.bb8.reparentTo(render)
+        #self.bb8.setPos(64.199, -98.559, -14.481)
+        #self.bb8.setScale(0.25)
+        #self.bb8.setHpr(-349.820, 0, 0)
 
-        self.bb8Move = Sequence(
-            LerpPosInterval(self.bb8, 5, (-19.337, -98.489, -14.485)),
-            Wait(0.7),
-            LerpPosInterval(self.bb8, 5, (-48.061, -38.341, -14.649)),
-            Wait(0.7),
-            LerpPosInterval(self.bb8, 5, (-58.508, 19.285, -14.489)),
-            Wait(0.7),
-            LerpPosInterval(self.bb8, 5, (-19.175, 58.556, -14.484)),
-            Wait(0.7),
-            LerpPosInterval(self.bb8, 5, (64.301, 58.495, -14.481)),
-            Wait(0.7),
-            LerpPosInterval(self.bb8, 5, (51.671, -13.673, -14.593)),
-            Wait(0.7),
-            LerpPosInterval(self.bb8, 5, (64.199, -98.559, -14.481))
-        )
-        self.bb8Move.loop()
+        #self.bb8Move = Sequence(
+        #    LerpPosInterval(self.bb8, 5, (-19.337, -98.489, -14.485)),
+        #    Wait(0.7),
+        #    LerpPosInterval(self.bb8, 5, (-48.061, -38.341, -14.649)),
+        #    Wait(0.7),
+        #    LerpPosInterval(self.bb8, 5, (-58.508, 19.285, -14.489)),
+        #    Wait(0.7),
+        #    LerpPosInterval(self.bb8, 5, (-19.175, 58.556, -14.484)),
+        #    Wait(0.7),
+        #    LerpPosInterval(self.bb8, 5, (64.301, 58.495, -14.481)),
+        #    Wait(0.7),
+        #    LerpPosInterval(self.bb8, 5, (51.671, -13.673, -14.593)),
+        #    Wait(0.7),
+        #    LerpPosInterval(self.bb8, 5, (64.199, -98.559, -14.481))
+        #)
+        #self.bb8Move.loop()
 
     def unload(self):
         SafeZoneLoader.SafeZoneLoader.unload(self)
         del self.piano
-        self.blueEyes.removeNode()
-        del self.blueEyes
-        self.bb8.removeNode()
-        del self.bb8
+        #self.blueEyes.removeNode()
+        #del self.blueEyes
+        #self.bb8.removeNode()
+        #del self.bb8
