4bac0619ba48fb6a379011e1297b87d50ff88b8a
Sketched <34355327+neatsketched@users.noreply.github.com>
2019-12-15 13:23:41 -0600

    Cogs: Tweaked lots of attacks

--- toontown/suit/Suit.py
+++ toontown/suit/Suit.py
@@ -70,11 +70,10 @@ hh = (('pen-squirt', 'fountain-pen', 7),
  ('throw-paper', 'throw-paper', 5),
  ('magic1', 'magic1', 5),
  ('roll-o-dex', 'roll-o-dex', 5))
-cr = (('pickpocket', 'pickpocket', 5), ('throw-paper', 'throw-paper', 3.5), ('glower', 'glower', 5))
-tbc = (('cigar-smoke', 'cigar-smoke', 8),
- ('glower', 'glower', 5),
- ('song-and-dance', 'song-and-dance', 8),
- ('golf-club-swing', 'golf-club-swing', 5))
+cr = (('pickpocket', 'pickpocket', 5), ('throw-paper', 'throw-paper', 3.5), ('glower', 'glower', 5), ('magic1', 'magic1', 5))
+tbc = (('glower', 'glower', 5),
+ ('golf-club-swing', 'golf-club-swing', 5),
+ ('magic2', 'magic2', 5))
 cc = (('speak', 'speak', 5),
  ('glower', 'glower', 5),
  ('phone', 'phone', 3.5),
@@ -115,7 +114,7 @@ bc = (('phone', 'phone', 5), ('hold-pencil', 'hold-pencil', 5))
 nc = (('phone', 'phone', 5), ('throw-object', 'throw-object', 5))
 mb = (('magic1', 'magic1', 5), ('throw-paper', 'throw-paper', 3.5))
 ls = (('throw-paper', 'throw-paper', 5), ('throw-object', 'throw-object', 5), ('hold-pencil', 'hold-pencil', 5))
-rb = (('glower', 'glower', 5), ('magic1', 'magic1', 5), ('golf-club-swing', 'golf-club-swing', 5))
+rb = (('glower', 'glower', 5), ('magic1', 'magic1', 5), ('golf-club-swing', 'golf-club-swing', 5), ('magic3', 'magic3', 5))
 bf = (('pickpocket', 'pickpocket', 5),
  ('rubber-stamp', 'rubber-stamp', 5),
  ('shredder', 'shredder', 3.5),
@@ -144,13 +143,15 @@ sd = (('magic2', 'magic2', 5),
 le = (('speak', 'speak', 5),
  ('throw-object', 'throw-object', 5),
  ('glower', 'glower', 5),
- ('throw-paper', 'throw-paper', 5))
+ ('throw-paper', 'throw-paper', 5),
+ ('magic1', 'magic1', 5))
 bw = (('finger-wag', 'fingerwag', 5),
  ('cigar-smoke', 'cigar-smoke', 8),
  ('gavel', 'gavel', 8),
  ('magic1', 'magic1', 5),
  ('throw-object', 'throw-object', 5),
- ('throw-paper', 'throw-paper', 5))
+ ('throw-paper', 'throw-paper', 5),
+ ('glower', 'glower', 5))
 if not base.config.GetBool('want-new-cogs', 0):
     ModelDict = {'a': ('/models/char/suitA-', 4),
      'b': ('/models/char/suitB-', 4),
