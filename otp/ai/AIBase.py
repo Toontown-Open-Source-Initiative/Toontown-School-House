7ec664425887c71ddcc2835fe58075247a635630
Vitor25laser <97393601+Vitor23laser@users.noreply.github.com>
2023-09-24 12:02:35 -0300

    Update AIBase.py

--- otp/ai/AIBase.py
+++ otp/ai/AIBase.py
@@ -1,16 +1,18 @@
 from panda3d.core import *
+from panda3d.direct import *
 from direct.directnotify.DirectNotifyGlobal import *
 from direct.showbase.MessengerGlobal import *
 from direct.showbase.BulletinBoardGlobal import *
 from direct.task.TaskManagerGlobal import *
 from direct.showbase.JobManagerGlobal import *
 from direct.showbase.EventManagerGlobal import *
-from direct.showbase.PythonUtil import *
-from direct.showbase import PythonUtil
+from otp.otpbase.PythonUtil import *
+from otp.otpbase import PythonUtil
 from direct.interval.IntervalManager import ivalMgr
 from direct.task import Task
 from direct.showbase import EventManager
 from direct.showbase import ExceptionVarDump
+from direct.showbase import DConfig
 import math
 import sys
 import time
