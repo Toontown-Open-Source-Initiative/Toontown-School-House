a1239137018ad13bf4b5daac6eb183857b639edf
Vitor25laser <97393601+Vitor23laser@users.noreply.github.com>
2023-10-29 20:30:29 -0300

    Update AIBase.py

--- otp/ai/AIBase.py
+++ otp/ai/AIBase.py
@@ -20,19 +20,18 @@ import gc
 
 class AIBase:
     notify = directNotify.newCategory('AIBase')
-    
+
     def __init__(self):
-        self.config = getConfigShowbase()
+        self.config = DConfig
         __builtins__['__dev__'] = self.config.GetBool('want-dev', 0)
-        if self.config.GetBool('want-variable-dump', 0):
+         if self.config.GetBool('want-variable-dump', 0):
             ExceptionVarDump.install()
-        
         if self.config.GetBool('use-vfs', 1):
             vfs = VirtualFileSystem.getGlobalPtr()
         else:
             vfs = None
         self.wantTk = self.config.GetBool('want-tk', 0)
-        self.AISleep = self.config.GetFloat('ai-sleep', 0.040000000000000001)
+        self.AISleep = self.config.GetFloat('ai-sleep', 0.04)
         self.AIRunningNetYield = self.config.GetBool('ai-running-net-yield', 0)
         self.AIForceSleep = self.config.GetBool('ai-force-sleep', 0)
         self.eventMgr = eventMgr
@@ -65,23 +64,18 @@ class AIBase:
         defaultValue = 1
         if __dev__:
             defaultValue = 0
-        
         wantFakeTextures = self.config.GetBool('want-fake-textures-ai', defaultValue)
         if wantFakeTextures:
             loadPrcFileData('aibase', 'textures-header-only 1')
-        
         self.wantPets = self.config.GetBool('want-pets', 1)
         if self.wantPets:
             if game.name == 'toontown':
-                PetConstants = PetConstants
-                import toontown.pets
+                from toontown.pets import PetConstants
                 self.petMoodTimescale = self.config.GetFloat('pet-mood-timescale', 1.0)
                 self.petMoodDriftPeriod = self.config.GetFloat('pet-mood-drift-period', PetConstants.MoodDriftPeriod)
                 self.petThinkPeriod = self.config.GetFloat('pet-think-period', PetConstants.ThinkPeriod)
                 self.petMovePeriod = self.config.GetFloat('pet-move-period', PetConstants.MovePeriod)
                 self.petPosBroadcastPeriod = self.config.GetFloat('pet-pos-broadcast-period', PetConstants.PosBroadcastPeriod)
-            
-        
         self.wantBingo = self.config.GetBool('want-fish-bingo', 1)
         self.wantKarts = self.config.GetBool('wantKarts', 1)
         self.newDBRequestGen = self.config.GetBool('new-database-request-generate', 1)
@@ -95,7 +89,6 @@ class AIBase:
         self.createStats()
         self.restart()
 
-    
     def setupCpuAffinities(self, minChannel):
         if game.name == 'uberDog':
             affinityMask = self.config.GetInt('uberdog-cpu-affinity-mask', -1)
@@ -109,12 +102,10 @@ class AIBase:
                 affinity = self.config.GetInt('uberdog-cpu-affinity', -1)
                 if autoAffinity and affinity == -1:
                     affinity = 2
-                
             else:
                 affinity = self.config.GetInt('ai-cpu-affinity', -1)
                 if autoAffinity and affinity == -1:
                     affinity = 1
-                
             if affinity != -1:
                 TrueClock.getGlobalPtr().setCpuAffinity(1 << affinity)
             elif autoAffinity:
@@ -123,82 +114,61 @@ class AIBase:
                     channelSet -= 240
                     affinity = channelSet + 3
                     TrueClock.getGlobalPtr().setCpuAffinity(1 << affinity % 4)
-                
-            
 
-    
     def taskManagerDoYield(self, frameStartTime, nextScheuledTaksTime):
         minFinTime = frameStartTime + self.MaxEpockSpeed
         if nextScheuledTaksTime > 0 and nextScheuledTaksTime < minFinTime:
             minFinTime = nextScheuledTaksTime
-        
         delta = minFinTime - globalClock.getRealTime()
         while delta > 0.002:
             time.sleep(delta)
             delta = minFinTime - globalClock.getRealTime()
 
-    
     def createStats(self, hostname = None, port = None):
         if not self.wantStats:
             return False
-        
         if PStatClient.isConnected():
             PStatClient.disconnect()
-        
         if hostname is None:
             hostname = ''
-        
         if port is None:
             port = -1
-        
         PStatClient.connect(hostname, port)
         return PStatClient.isConnected()
 
-    
-    def _AIBase__sleepCycleTask(self, task):
+    def __sleepCycleTask(self, task):
         time.sleep(self.AISleep)
         return Task.cont
 
-    
-    def _AIBase__resetPrevTransform(self, state):
+    def __resetPrevTransform(self, state):
         PandaNode.resetAllPrevTransform()
         return Task.cont
 
-    
-    def _AIBase__ivalLoop(self, state):
+    def __ivalLoop(self, state):
         ivalMgr.step()
         return Task.cont
 
-    
-    def _AIBase__igLoop(self, state):
+    def __igLoop(self, state):
         self.graphicsEngine.renderFrame()
         return Task.cont
 
-    
     def shutdown(self):
         self.taskMgr.remove('ivalLoop')
         self.taskMgr.remove('igLoop')
         self.taskMgr.remove('aiSleep')
         self.eventMgr.shutdown()
 
-    
     def restart(self):
         self.shutdown()
-        self.taskMgr.add(self._AIBase__resetPrevTransform, 'resetPrevTransform', priority = -51)
-        self.taskMgr.add(self._AIBase__ivalLoop, 'ivalLoop', priority = 20)
-        self.taskMgr.add(self._AIBase__igLoop, 'igLoop', priority = 50)
-        if self.AISleep >= 0:
-            if not (self.AIRunningNetYield) or self.AIForceSleep:
-                self.taskMgr.add(self._AIBase__sleepCycleTask, 'aiSleep', priority = 55)
-            
+        self.taskMgr.add(self.__resetPrevTransform, 'resetPrevTransform', priority=-51)
+        self.taskMgr.add(self.__ivalLoop, 'ivalLoop', priority=20)
+        self.taskMgr.add(self.__igLoop, 'igLoop', priority=50)
+        if self.AISleep >= 0 and (not self.AIRunningNetYield or self.AIForceSleep):
+            self.taskMgr.add(self.__sleepCycleTask, 'aiSleep', priority=55)
         self.eventMgr.restart()
 
-    
     def getRepository(self):
         return self.air
 
-    
     def run(self):
         self.taskMgr.run()
-
-
